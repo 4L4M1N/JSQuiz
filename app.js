ee213b78e50d9ca474b3b82c7e20e56f26a67a54
4L4M1N <mohammadamin601@gmail.com>
2019-07-01 13:20:06 +0600

    result

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const correctAnswers = ['B', 'B', 'B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value];
+    userAnswers.forEach((answer, index) =>{
+        if(answer === correctAnswers[index]){
+            score +=25;
+        }
+    });
+    //console.log(score);
+    result.querySelector('span').textContent = `${score}%`;
+ 
+    result.classList.remove('d-none');
+});
